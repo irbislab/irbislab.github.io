fa26eb2b9503df9281b84ac37ff2bd73508b6cf9
Gustavo Leguizamon <leguizamon.gustavo@gmail.com>
2013-09-23 02:40:54 -0300

    New design

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,7 +3,7 @@
 
   w.log = function(){
     if (window.console && console.log) {
-      var a = ['[irbis]'].concat([].slice.call(arguments));
+      var a = ['[IL]'].concat([].slice.call(arguments));
       console.log.apply(console, a);
     }
   };
@@ -23,44 +23,28 @@
   };
 
   w.getWindowSize = function(){
-    var w = window
-      , d = document
-      , e = d.documentElement
-      , g = d.getElementsByTagName('body')[0]
-      , width = w.innerWidth || e.clientWidth || g.clientWidth
-      , height = w.innerHeight || e.clientHeight || g.clientHeight;
-
+    var w = window;
+    var d = document;
+    var e = d.documentElement;
+    var g = d.getElementsByTagName('body')[0];
+    var width = w.innerWidth || e.clientWidth || g.clientWidth;
+    var height = w.innerHeight || e.clientHeight || g.clientHeight;
     return { height: height, width: width };
   };
 
   w.getHeight = function(el){
-    var borderT = parseFloat(el.style.borderTopWidth) || 0
-      , borderB = parseFloat(el.style.borderBottomWidth) || 0
-      , paddingT = parseFloat(el.style.paddingTop) || 0
-      , paddingB = parseFloat(el.style.paddingBottom) || 0;
-
+    var borderT = parseFloat(el.style.borderTopWidth) || 0;
+    var borderB = parseFloat(el.style.borderBottomWidth) || 0;
+    var paddingT = parseFloat(el.style.paddingTop) || 0;
+    var paddingB = parseFloat(el.style.paddingBottom) || 0;
     return el.offsetHeight - borderT - borderB - paddingT - paddingB;
   };
 
-  w.getScrollY = function() {
+  w.getScrollY = function(){
     return (window.pageYOffset !== undefined) ? window.pageYOffset :
-      (document.documentElement || document.body.parentNode || document.body).scrollTop
+      (document.documentElement || document.body.parentNode || document.body)
+      .scrollTop;
   }
-
-  w.validateInput = function(i){
-    var error = 0;
-    if (i.classList.contains('required')) {
-      if (i.value.length === 0
-          || (i.type.indexOf('select') === 0
-              && i.options[i.selectedIndex].value === '')) {
-        i.classList.add('error');
-        error = 1;
-      } else {
-        i.classList.remove('error');
-      }
-    }
-    return error;
-  };
 })(window);
 
 (function(bean) {
@@ -79,88 +63,15 @@
 
 domready(function(){
   if (document.documentElement.classList.contains('touch')) {
-    var toggleLabel = qs('label[for=nav-toggle]')
-      , toggleInput = qid('nav-toggle')
-      , tapToggle = new Tap(toggleLabel);
+    var toggleLabel = qs('label[for=nav-toggle]');
+    var toggleInput = qid('nav-toggle');
+    var tapToggle = new Tap(toggleLabel);
 
     bean.on(toggleLabel, 'click', function(e){
       e.preventDefault();
     });
     bean.on(toggleLabel, 'tap', function(e){
       toggleInput.checked = !toggleInput.checked;
-    });
-  }
-
-  if (getScrollY() < 160 && getWindowSize().width < 680) {
-    qs('.logo > img').src = 'images/iso.png';
-  }
-
-  bean.on(window, 'resize', function(){
-    var header = qtag('header')[0]
-      , hs = qid('hs')
-      , winWidth = getWindowSize().width;
-
-    if (winWidth <= 680) {
-      qs('.logo > img').src = 'images/iso.png';
-    }
-
-    if (winWidth < 768) {
-      header.style.position = 'static';
-      hs.style.paddingTop = '6.2em';
-    } else {
-      qs('.logo > img').src = 'images/iso-blast.png';
-    }
-  });
-
-  bean.on(window, 'scroll', function(){
-    var header = qtag('header')[0]
-      , hs = qid('hs')
-      , logo = qs('.logo > img')
-      , winHeight = getWindowSize().height
-      , winWidth = getWindowSize().width
-      , winScrollY = getScrollY();
-
-    if (winWidth > 768) {
-      header.style.position = 'fixed';
-
-      if (document.body.id === 'section') {
-        hs.style.paddingTop = '7.6em';
-      } else {
-        hs.style.paddingTop = '11.6em';
-      }
-
-      if (winScrollY > 160) {
-        header.classList.add('fixed');
-        logo.src = 'images/iso.png';
-      } else if (winScrollY <= 0) {
-        header.style.position = 'static';
-
-        if (document.body.id === 'section') {
-          hs.style.paddingTop = '2.2em';
-        } else {
-          hs.style.paddingTop = '6.2em';
-        }
-
-        logo.src = 'images/iso-blast.png';
-        header.classList.remove('fixed');
-      } else {
-        header.classList.remove('fixed');
-        logo.src = 'images/iso-blast.png';
-      }
-    } else {
-      header.style.position = 'static';
-    }
-  });
-
-  var cf = qs('form[method="post"]');
-  if (cf) {
-    bean.on(cf, 'submit', function(e){
-      var f = e.target
-        , error = 0;
-      q('input, textarea', f).forEach(function(x){
-        if (validateInput(x)) error = 1;
-      });
-      if (error) e.preventDefault();
     });
   }
 
@@ -174,4 +85,9 @@ domready(function(){
       });
     });
   }
+
+  $('a[href^="#"]').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $(this.hash).offset().top - 58 }, 300);
+  });
 });
--- javascripts/lib.js
+++ javascripts/lib.js
@@ -9,3 +9,104 @@
 
 // classList
 if(typeof document!=="undefined"&&!("classList" in document.createElement("a"))){(function(j){if(!("HTMLElement" in j)&&!("Element" in j)){return}var a="classList",f="prototype",m=(j.HTMLElement||j.Element)[f],b=Object,k=String[f].trim||function(){return this.replace(/^\s+|\s+$/g,"")},c=Array[f].indexOf||function(q){var p=0,o=this.length;for(;p<o;p++){if(p in this&&this[p]===q){return p}}return -1},n=function(o,p){this.name=o;this.code=DOMException[o];this.message=p},g=function(p,o){if(o===""){throw new n("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(o)){throw new n("INVALID_CHARACTER_ERR","String contains an invalid character")}return c.call(p,o)},d=function(s){var r=k.call(s.className),q=r?r.split(/\s+/):[],p=0,o=q.length;for(;p<o;p++){this.push(q[p])}this._updateClassName=function(){s.className=this.toString()}},e=d[f]=[],i=function(){return new d(this)};n[f]=Error[f];e.item=function(o){return this[o]||null};e.contains=function(o){o+="";return g(this,o)!==-1};e.add=function(){var s=arguments,r=0,p=s.length,q,o=false;do{q=s[r]+"";if(g(this,q)===-1){this.push(q);o=true}}while(++r<p);if(o){this._updateClassName()}};e.remove=function(){var t=arguments,s=0,p=t.length,r,o=false;do{r=t[s]+"";var q=g(this,r);if(q!==-1){this.splice(q,1);o=true}}while(++s<p);if(o){this._updateClassName()}};e.toggle=function(p,q){p+="";var o=this.contains(p),r=o?q!==true&&"remove":q!==false&&"add";if(r){this[r](p)}return o};e.toString=function(){return this.join(" ")};if(b.defineProperty){var l={get:i,enumerable:true,configurable:true};try{b.defineProperty(m,a,l)}catch(h){if(h.number===-2146823252){l.enumerable=false;b.defineProperty(m,a,l)}}}else{if(b[f].__defineGetter__){m.__defineGetter__(a,i)}}}(self))};
+
+/*
+ *
+ * Find more about this plugin by visiting
+ * http://alxgbsn.co.uk/
+ *
+ * Copyright (c) 2013 Alex Gibson, http://alxgbsn.co.uk/
+ * Released under MIT license
+ *
+ */
+
+(function (window, document) {
+  'use strict';
+
+  function Tap(el) {
+    this.element = typeof el === 'object' ? el : document.getElementById(el);
+    this.moved = false; //flags if the finger has moved
+    this.startX = 0; //starting x coordinate
+    this.startY = 0; //starting y coordinate
+    this.hasTouchEventOccured = false; //flag touch event
+    this.element.addEventListener('touchstart', this, false);
+    this.element.addEventListener('mousedown', this, false);
+  }
+
+  //start
+  Tap.prototype.start = function (e) {
+    if (e.type === 'touchstart') {
+      this.hasTouchEventOccured = true;
+    }
+    this.moved = false;
+    this.startX = e.type === 'touchstart' ? e.touches[0].clientX : e.clientX;
+    this.startY = e.type === 'touchstart' ? e.touches[0].clientY : e.clientY;
+    this.element.addEventListener('touchmove', this, false);
+    this.element.addEventListener('touchend', this, false);
+    this.element.addEventListener('touchcancel', this, false);
+    this.element.addEventListener('mousemove', this, false);
+    this.element.addEventListener('mouseup', this, false);
+  };
+
+  //move
+  Tap.prototype.move = function (e) {
+    var x = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX,
+      y = e.type === 'touchmove' ? e.touches[0].clientY : e.clientY;
+
+    //if finger moves more than 10px flag to cancel
+    if (Math.abs(x - this.startX) > 10 || Math.abs(y - this.startY) > 10) {
+      this.moved = true;
+    }
+  };
+
+  //end
+  Tap.prototype.end = function (e) {
+    var evt;
+
+    if (this.hasTouchEventOccured && e.type === 'mouseup') {
+      e.preventDefault();
+      e.stopPropagation();
+      this.hasTouchEventOccured = false;
+      return;
+    }
+
+    if (!this.moved) {
+      evt = document.createEvent('Event');
+      evt.initEvent('tap', true, true);
+      e.target.dispatchEvent(evt);
+    }
+    this.element.removeEventListener('touchmove', this, false);
+    this.element.removeEventListener('touchend', this, false);
+    this.element.removeEventListener('touchcancel', this, false);
+    this.element.removeEventListener('mousemove', this, false);
+    this.element.removeEventListener('mouseup', this, false);
+  };
+
+  //touchcancel
+  Tap.prototype.cancel = function (e) {
+    //reset state
+    this.moved = false;
+    this.startX = 0;
+    this.startY = 0;
+    this.element.removeEventListener('touchmove', this, false);
+    this.element.removeEventListener('touchend', this, false);
+    this.element.removeEventListener('touchcancel', this, false);
+    this.element.removeEventListener('mousemove', this, false);
+    this.element.removeEventListener('mouseup', this, false);
+  };
+
+  Tap.prototype.handleEvent = function (e) {
+    switch (e.type) {
+    case 'touchstart': this.start(e); break;
+    case 'touchmove': this.move(e); break;
+    case 'touchend': this.end(e); break;
+    case 'touchcancel': this.cancel(e); break;
+    case 'mousedown': this.start(e); break;
+    case 'mousemove': this.move(e); break;
+    case 'mouseup': this.end(e); break;
+    }
+  };
+
+  //public function
+  window.Tap = Tap;
+}(window, document));
